58de978838bcc52862a2a6e05573dd689a06a143
navelar <heldercsousa@gmail.com>
2019-05-22 16:34:35 -0300

    continuação curso

---
+++ Angular7UdemyMaximillianCompleteGuideCourseProject/src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+    /*public name: string;
+    public amount: number;
+    
+    constructor(name: string, amount: number) {
+        this.name=name;
+        this.amount=amount;
+    }
+    below is A SHORTCUT TO ABOVE APPROACH
+    BEHIND THE SCENES, below BUILDS THE SAME AS ABOVE
+    */
+   
+   constructor(public name: string, public amount:number) {} 
+}
