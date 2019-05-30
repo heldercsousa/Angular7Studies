633f7848a9cefc4b963873caec77511d1d7907bd
navelar <heldercsousa@gmail.com>
2019-05-30 10:00:03 -0300

    ultimass alteracoes

--- Angular7UdemyMaximillianCompleteGuideCourseProject/src/app/header/header.component.ts
+++ Angular7UdemyMaximillianCompleteGuideCourseProject/src/app/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component ({
  selector: 'app-header',
  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() linkClicked = new EventEmitter<number>();
+
+    onLinkClick(idx: number) {
+      this.linkClicked.emit(idx);
+      console.log("idx clicado:" + idx);
+    }
+  
 }
