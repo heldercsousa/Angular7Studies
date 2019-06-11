b60bde1c15e6b7f7d7c98a19de35f338894853f9
DESKTOPHELDER\Usuario <heldercsousa@gmail.com>
2019-06-11 20:47:46 -0300

    Section 12 - Memory Leak example in practice

--- Section12Observables/src/app/home/home.component.ts
+++ Section12Observables/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs'; //Observable actually are provided exclusively by rxjs library. Thats neither js nor angular
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private firstObsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = //stores the subscription, not the observable 
+    interval(1000) //returns an Observable which fires on each second. Thats not an entire observable made from scratch, but it´s close.
+    .subscribe( count => {
+      console.log(count);
+    })
   }
 
 }
