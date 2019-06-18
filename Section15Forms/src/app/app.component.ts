409855d621367aec8bdbfadb2c3fa208cbbfc488
DESKTOPHELDER\Usuario <heldercsousa@gmail.com>
2019-06-18 19:56:05 -0300

    Section 15 Template-driven forms -- Property Binding to assign defaulValues and bind to a class property.

--- Section15Forms/src/app/app.component.ts
+++ Section15Forms/src/app/app.component.ts
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', { static: false}) signupForm: NgForm; //alternative approach to onSubmit(form: NgForm)
-
+  defaultQuestion = 'pet';
+  answer = "";
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
