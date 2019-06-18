bbe4064df4d18704c3e63542de79ed68e9d39587
DESKTOPHELDER\Usuario <heldercsousa@gmail.com>
2019-06-17 22:50:45 -0300

    Section 15 Forms - upload start project

---
+++ Section15Forms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm; //alternative approach to onSubmit(form: NgForm)
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
