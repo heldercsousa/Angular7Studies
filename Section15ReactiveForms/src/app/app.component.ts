2468d8dd7cddff1dfc8ee301ed879120d56fc652
heldercsousa <heldercsousa@gmail.com>
2019-06-24 15:25:15 -0300

    Section 15 Reactive-driven forms  -  passing default values for the form

--- Section15ReactiveForms/src/app/app.component.ts
+++ Section15ReactiveForms/src/app/app.component.ts
@@ -33,6 +33,15 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (value)=>console.log(value)
     );
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
   }
 
   onSubmit() {
