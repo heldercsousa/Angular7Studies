eee2b0f955113e8f4527f28c7e799ff5831e8548
heldercsousa <heldercsousa@gmail.com>
2019-06-24 12:59:26 -0300

    Section 15 Reactive-driven forms - adding validation

--- Section15ReactiveForms/src/app/app.component.ts
+++ Section15ReactiveForms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
     //property is wrapped up with cotation mark just to be safe it will be kept like this when minification runs
     //it might not be necessary depending on the case
     this.signupForm = new FormGroup({
-      'username' : new FormControl(null),
-      'email' : new FormControl(null),
+      'username' : new FormControl(null, Validators.required),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
       'gender' : new FormControl('male')
     });
   }
