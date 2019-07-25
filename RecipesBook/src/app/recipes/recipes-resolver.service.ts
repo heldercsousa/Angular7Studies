4f7c731764cbb8c1024d8766dac49baba1131051
heldercsousa <heldercsousa@gmail.com>
2019-07-25 14:34:42 -0300

    Section 19 Http in recipes app - avoiding data lost in Resolver

--- RecipesBook/src/app/recipes/recipes-resolver.service.ts
+++ RecipesBook/src/app/recipes/recipes-resolver.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStore: DataStorageService) { }
+  constructor(private dataStore: DataStorageService, private recipesService: RecipeService) { }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStore.fetchRecipes(); //we dont subscribe here cause resolver will do it for us.
+    const recipes = this.recipesService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStore.fetchRecipes(); //we dont subscribe here cause resolver will do it for us. 
+    }
+    else {
+      return recipes; //avoid data lost when it already exists.
+    }
   }
 }
