93b0909c10247afa74b292ce45345df596bc7c50
heldercsousa <heldercsousa@gmail.com>
2019-08-05 18:17:01 -0300

    Section 26 - Animations - a complexier one

--- Section26Animations/src/app/app.component.ts
+++ Section26Animations/src/app/app.component.ts
@@ -14,18 +14,41 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [ 
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'blue',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state =='normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState =='normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
+
+  onShrunken() {
+    this.wildState = 'shrunken';
+  }
+
   onAdd(item) {
     this.list.push(item);
   }
