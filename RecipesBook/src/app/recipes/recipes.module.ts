f7c550ec43676f3ab4dc89c7064ed3b6657f08b4
DESKTOPHELDER\Usuario <heldercsousa@gmail.com>
2020-02-11 22:32:11 -0200

    Section 22 - Modules & Optimization - Splitting modules correctly

--- RecipesBook/src/app/recipes/recipes.module.ts
+++ RecipesBook/src/app/recipes/recipes.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
@@ -14,6 +18,12 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeItemComponent,
     RecipeStartComponent,
     RecipeEditComponent,
+  ],
+  imports: [
+    RouterModule,
+    // BrowserModule // that´s not good, cause it imports lots of features beyond ngFor n ngIf.
+    CommonModule, // instead, we must import CommonModule, wich provides access to ngIf n ngFor
+    ReactiveFormsModule
   ],
   exports: [ // make components above available through other modules beyond RecipesModule. Without it, they would not be visible
     RecipesComponent,
