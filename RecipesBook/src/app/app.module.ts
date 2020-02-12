03237c7a02ed7ba827065397136205717d713d1c
DESKTOPHELDER\Usuario <heldercsousa@gmail.com>
2020-02-11 22:21:28 -0200

    Section 22 - Modules & Optimization - AppModule´s unavailable modules error on RecipesModule

--- RecipesBook/src/app/app.module.ts
+++ RecipesBook/src/app/app.module.ts
@@ -6,10 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -17,28 +13,21 @@ import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    HeaderComponent, 
-    RecipesComponent,
-    RecipeListComponent, 
-    RecipeDetailComponent, 
-    RecipeItemComponent, 
-    ShoppingListComponent, 
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -47,13 +36,22 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule, //has to be added otherwise Reactive-drivenb approach doesnt work
+    ReactiveFormsModule, // has to be added otherwise Reactive-drivenb approach doesnt work
     HttpClientModule,
     NgbModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RecipesModule // make this RecipesModule available in AppModule
   ],
-  providers: [ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }], //it makes sure all app has the same RecipeService instance, avoiding instantiating it more than once
+  providers: [// it makes sure all app has the same RecipeService instance, avoiding instantiating it more than once
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
   entryComponents: [
     AlertComponent // now Angular is ready to create this component whenever it is prompted to creation. Without this it´s not able to create it
